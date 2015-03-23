9eb3a8bee4a8720164b1d1e83d54a05ed2e43ca5
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-03-23 13:35:30 +0100

    Added Read vector example

---
+++ src/test/java/vector/Example_1_ReadVectors.java
@@ -0,0 +1,33 @@
+package vector;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.Query;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.junit.Test;
+
+import java.net.URL;
+import java.util.Arrays;
+
+/**
+ * Read a shapefile and print information about the file.
+ * @author Jesse on 3/23/2015.
+ */
+public class Example_1_ReadVectors {
+    @Test
+    public void testRead() throws Exception {
+        final ShapefileDataStoreFactory factory = new ShapefileDataStoreFactory();
+
+        final URL url = getClass().getResource("/france.shp");
+        final FileDataStore dataStore = factory.createDataStore(url);
+
+        final SimpleFeatureSource featureSource = dataStore.getFeatureSource(dataStore.getTypeNames()[0]);
+
+        final ReferencedEnvelope bounds = featureSource.getBounds();
+
+        System.out.println("Feature Types:" + Arrays.toString(dataStore.getTypeNames()));
+        System.out.printf("Data Bounding Box: [%s, %s  %s, %s] \n", bounds.getMinX(), bounds.getMinY(),  bounds.getMaxX(),  bounds.getMaxY());
+        System.out.println("Number of features in file: " + featureSource.getCount(Query.ALL));
+    }
+}
